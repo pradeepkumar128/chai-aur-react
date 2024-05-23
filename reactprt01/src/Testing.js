b3b60edd3c9e3be2cc0b3a4377e2ae793a8cb0cb
pradeepkumar128 <iampradeep1282@gmail.com>
2024-05-23 23:18:40 +0530

    third commit

--- reactprt01/src/Testing.js
+++ reactprt01/src/Testing.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+const username = 'Pradeep Prajapati';
 const Testing = () => {
   return (
     <>
-    <h1>Testing in React </h1>
+    <h1>Testing in React | {username} </h1>
     </>
   )
 }
